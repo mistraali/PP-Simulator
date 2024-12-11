4fc6e0c978d15dc9ba30874444ab7ab9562a895a
mistraali <bartlomiejkapusta@gmail.com>
2024-12-11 22:20:13 +0100

    Stwory

---
+++ Simulator/Animals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+    internal class Animals
+    {
+        public required string Description { get; init; }
+        public uint Size { get; set; } = 3;
+
+        public string Info => $"{Description} <{Size}>";
+    }
+}
