40ebed0d174626ca0164032a3c7e67b63198060e
mistraali <bartlomiejkapusta@gmail.com>
2024-12-11 23:32:50 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,16 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Simulator
 {
     internal class Animals
     {
-        public required string Description { get; init; }
-        public uint Size { get; set; } = 3;
+        private string _description = "Unknown";
+        private uint size = 3;
 
+        public required string Description
+        {
+            get => _description;
+            init
+            {
+                int minLength = 3;
+                int maxLength = 15;
+                _description = value;
+                //trimming
+                _description = _description.Trim();
+                //cutting long descriptions
+                if (_description.Length > maxLength)
+                {
+                    _description = _description.Substring(0, maxLength);
+                    _description = _description.Trim();
+                }
+                //filling short descriptions
+                if (_description.Length < minLength)
+                {
+                    do
+                    {
+                        _description += '#';
+                    } while (_description.Length < minLength);
+                }
+                //descriptions should start with upper case
+                _description = char.ToUpper(_description[0]) + _description.Substring(1);
+            }
+        }
+        public uint Size { get => size; set => size = value; }
         public string Info => $"{Description} <{Size}>";
     }
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -9,8 +9,50 @@ namespace Simulator
 {
     internal class Creature
     {
-        public string Name { get; set; }
-        public int Level { get; set; }
+        //fields
+        private string _name = "Unknown";
+        private int _level = 1;
+        //properties
+        public string Name { 
+            get => _name; 
+            init
+            {
+                int minLength = 3;
+                int maxLength = 25;
+                _name = value;
+                //trimming
+                _name = _name.Trim();
+                //cutting long names
+                if (_name.Length > maxLength)
+                {
+                    _name = _name.Substring(0, maxLength);
+                    _name = _name.Trim();
+                }
+                //filling short names
+                if (_name.Length < minLength)
+                {
+                    do
+                    {
+                        _name += '#';
+                    } while (_name.Length < minLength);
+                }
+                //names should start with upper case
+                _name = char.ToUpper(_name[0]) + _name.Substring(1);
+            } 
+        }
+        public int Level 
+        { 
+            get => _level; 
+            init
+            {
+                //fitting into brackets
+                _level = value;
+                _level = _level < 1 ? 1 : _level;
+                _level = _level > 10 ? 10 : _level;
+            } 
+        }
+
+        public string Info => $"{Name} [{Level}]";
 
         public Creature(string name, int level = 1)
         {
@@ -20,8 +62,13 @@ namespace Simulator
 
         public Creature() { }
 
+        //write greeting to console
         public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
 
-        public string Info => $"{Name} [{Level}]";
+        //advance by 1 level
+        public void Upgrade()
+        {
+            if (Level < 10) _level++;
+        }
     }
 }
