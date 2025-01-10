fc437cedb177dbcb08e3a10651e417b11ac18944
mistraali <bartlomiejkapusta@gmail.com>
2025-01-10 14:03:31 +0100

    TDD

--- Simulator/Maps/SmallTorusMap.cs
+++ Simulator/Maps/SmallTorusMap.cs
@@ -1,36 +1,32 @@
 ﻿namespace Simulator.Maps;
 
-public class SmallSquareMap : Map
+public class SmallTorusMap : Map
 {
     public readonly int Size;
-
-    public SmallSquareMap(int size)
+    public SmallTorusMap(int size)
     {
         if (5 <= size && size <= 20) Size = size;
         else throw new ArgumentOutOfRangeException("Pick size between 5 and 20!");
     }
-
     public override bool Exist(Point p)
     {
-        if (new Rectangle(new Point(0,0), new Point(Size-1,Size-1)).Contains(p)) 
+        if (new Rectangle(new Point(0, 0), new Point(Size - 1, Size - 1)).Contains(p))
             return true;
-        else 
+        else
             return false;
     }
 
     public override Point Next(Point p, Direction d)
     {
+        if (!Exist(p)) return p;
         Point np = p.Next(d);
-        if (Exist(np)) 
-            return np;
-        else return p;
+        return new Point((np.X+20) % Size, (np.Y+20) % Size);
     }
 
     public override Point NextDiagonal(Point p, Direction d)
     {
+        if (!Exist(p)) return p;
         Point np = p.NextDiagonal(d);
-        if (Exist(np))
-            return np;
-        else return p;
+        return new Point((np.X + 20) % Size, (np.Y + 20) % Size);
     }
 }
