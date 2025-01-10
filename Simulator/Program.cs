f5451ec113a1133efc5db8ae3e54e69c5dcd3ce8
mistraali <bartlomiejkapusta@gmail.com>
2025-01-10 11:44:57 +0100

    Prostokąty

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,59 +5,52 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
-        //Lab4a();
-        //Lab4b();
+        Lab5a();
     }
-
-    static void Lab4a()
+    static void Lab5a()
     {
-        Console.WriteLine("HUNT TEST\n");
-        var o = new Orc() { Name = "Gorbag", Rage = 7 };
-        o.SayHi();
-        for (int i = 0; i < 10; i++)
+        var createRectangle = new Action<List<Object>>(points =>
         {
-            o.Hunt();
-            o.SayHi();
-        }
+            try
+            {
+                if (points.Count == 2)
+                    Console.WriteLine(new Rectangle((Point)points[0], (Point)points[1]));
+                else if (points.Count == 4)
+                    Console.WriteLine(new Rectangle((int)points[0], (int)points[1], (int)points[2], (int)points[3]));
+                else
+                    throw new ArgumentException();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Invalid points!");
+            }
+        });
 
-        Console.WriteLine("\nSING TEST\n");
-        var e = new Elf("Legolas", agility: 2);
-        e.SayHi();
-        for (int i = 0; i < 10; i++)
-        {
-            e.Sing();
-            e.SayHi();
-        }
+        int x1 = 0;
+        int y1 = 5;
+        int x2 = 2;
+        int y2 = 8;
+        List<Object> l0 = new List<Object> { x1, y1, x2, y2 };
+        createRectangle(l0);    //(0, 5):(2, 8)
 
-        Console.WriteLine("\nPOWER TEST\n");
-        Creature[] creatures = {
-        o,
-        e,
-        new Orc("Morgash", 3, 8),
-        new Elf("Elandor", 5, 3)
-    };
-        foreach (Creature creature in creatures)
-        {
-            Console.WriteLine($"{creature.Name,-15}: {creature.Power}");
-        }
-    }
+        Point p1 = new Point(0,5);
+        Point p2 = new Point(2,8);
+        List<Object> l1 = new List<Object> { p1, p2 };
+        createRectangle(l1);    //(0, 5):(2, 8)
 
-    static void Lab4b()
-    {
-        object[] myObjects = {
-        new Animals() { Description = "dogs"},
-        new Birds { Description = "  eagles ", Size = 10 },
-        new Elf("e", 15, -3),
-        new Orc("morgash", 6, 4),
-    };
-        Console.WriteLine("\nMy objects:");
-        foreach (var o in myObjects) Console.WriteLine(o);
-        /*
-            My objects:
-            ANIMALS: Dogs <3>
-            BIRDS: Eagles (fly+) <10>
-            ELF: E## [10][0]
-            ORC: Morgash [6][4]
-        */
+        Point p3 = new Point(0, 10);
+        Point p4 = new Point(2, 8);
+        List<Object> l2 = new List<Object> { p3, p4 };
+        createRectangle(l2);    //(0, 8):(2, 10)
+
+        Point p5 = new Point(0, 10);
+        Point p6 = new Point(2, 10);
+        List<Object> l3 = new List<Object> { p5, p6 };
+        createRectangle(l3);    //Invalid points!
+
+        Point p7 = new Point(2, 10);
+        Point p8 = new Point(0, 8);
+        List<Object> l4 = new List<Object> { p7, p8 };
+        createRectangle(l4);    //(0, 8):(2, 10)
     }
 }
---
+++ Simulator/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿namespace Simulator;
+
+internal class Rectangle
+{
+    public readonly int X1, Y1, X2, Y2;
+
+    public Rectangle(int x1, int y1, int x2, int y2)
+    {
+        if (x1 == x2 || y1 == y2)
+            throw new ArgumentException("Chosen points are collinear!");
+        else
+        {
+            X1 = Math.Min(x1, x2);
+            Y1 = Math.Min(y1, y2);
+            X2 = Math.Max(x2, x1);
+            Y2 = Math.Max(y2, y1);
+        }
+    }
+
+    public Rectangle(Point p1, Point p2) : this(p1.X, p1.Y, p2.X, p2.Y) { }
+
+    public override string ToString() => $"({X1}, {Y1}):({X2}, {Y2})";
+
+    public bool Contains(Point point)
+    {
+        if (X1 <= point.X && point.X <= X2 && Y1 <= point.Y && point.Y <= Y2) return true;
+        else return false;
+    }
+}
